b5e883d188be8aa29f6de8d993c8d04b6f60640e
“Codillac <luk.krolikowski@gmail.com>
2017-08-29 18:44:38 +0200

    Rename file

--- src/main/java/com/kalkulator/CalculatorApplication.java
+++ src/main/java/com/kalkulator/CalculatorApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KalkulatorApplication {
+public class CalculatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KalkulatorApplication.class, args);
+		SpringApplication.run(CalculatorApplication.class, args);
 	}
 }
